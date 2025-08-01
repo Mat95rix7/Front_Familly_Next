65dc7919883c3a6bdb7634dc34269d3ca5ec6c1f
Mat95rix7 <150734899+Mat95rix7@users.noreply.github.com>
2025-08-01 17:55:09 +0200

    Update page.js

--- app/familles/[id]/page.js
+++ app/familles/[id]/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
-import { apiFetch, getPhotoUrl } from "../../components/FetchAPI";
+import { apiFetch, getPhotoUrl } from "../../services/FetchAPI";
 import Image from "next/image";
 
 
